2292b1620dd703dedc36ec4f7f45123cb0107d6a
SHAZ1786 <zeven074@gmail.com>
2026-04-05 17:46:33 +0530

    .

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const categories = [
     {
         name: "Cotton Fabrics",
-        image: "/images/c1.Webp",
+        image: "/images/co1.Webp",
     },
     {
         name: "Silk Fabrics",
